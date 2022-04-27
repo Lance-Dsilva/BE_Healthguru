9e0f94e5b5399865b37065583161361f56296f1b
LanceLD <52831379+Lance-Dsilva@users.noreply.github.com>
2022-04-27 10:25:55 +0530

    Updates

---
+++ precache-manifest.5d2f50a85cb78cb038b51e5d8c2b1d58.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c918111f72a140d93f3d386de249a39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6282d2e3342898661294",
+    "url": "/static/css/2.1765e63e.chunk.css"
+  },
+  {
+    "revision": "65bffab5b582909c8dea",
+    "url": "/static/css/main.c13ffde2.chunk.css"
+  },
+  {
+    "revision": "6282d2e3342898661294",
+    "url": "/static/js/2.b99437e8.chunk.js"
+  },
+  {
+    "revision": "8c08c2421b89b38444da2a745cd837f8",
+    "url": "/static/js/2.b99437e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65bffab5b582909c8dea",
+    "url": "/static/js/main.353332d7.chunk.js"
+  },
+  {
+    "revision": "4792b66c7ea268d3b997",
+    "url": "/static/js/runtime-main.d43be887.js"
+  },
+  {
+    "revision": "df3badd7be8ec136c1e5c4be041eae24",
+    "url": "/static/media/background.df3badd7.jpg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  }
+]);
